439cf8d54a18b25a9ce90050cfbe0c0713a42cee
Joseph Wilk <joe@josephwilk.net>
2016-07-05 18:48:39 +0100

    no onsets == onset @ 0.0

--- src/sample_meta/aubio.clj
+++ src/sample_meta/aubio.clj
@@ -3,18 +3,33 @@
   (:require [clojure.java.shell :as shell]))
 
 (defn onsets [sample]
-  (let [o (shell/sh "aubioonset" sample)
-        out (clojure.string/split (:out o) #"\n")]
-    {:onsets out}))
+  (let [o (shell/sh "aubioonset" sample)]
+    (if (= (:exit o) 0)
+      (let [out (->> (clojure.string/split (:out o) #"\n")
+                     (map (fn [string]
+                            (if (clojure.string/blank? string)
+                              0.0
+                              (Double/parseDouble string)
+                              ))))]
+        {:onsets out})
+      (do
+        (println (:err o))
+        {:onsets []})
+      )))
 
 (defn notes [sample]
   (let [o (shell/sh "aubionotes" sample)
         [head & [tail]] (clojure.string/split (:out o) #"\n")
-        tails (clojure.string/split tail #"\t")]
+        tails (->> (clojure.string/split tail #"\t")
+                  (map (fn [string] (Double/parseDouble string))))]
     {:onset head
      :notes tails}))
 
 (comment
   (notes "/Users/josephwilk/Workspace/music/samples/Abstract/Loops/Melodic/Found Sound/120_C_Lamp_01_SP.wav")
   (onsets "/Users/josephwilk/Workspace/music/samples/Abstract/Loops/Melodic/Found Sound/120_C_Lamp_01_SP.wav")
+
+  (onsets "/Users/josephwilk/Workspace/music/samples/Abstract/Loops/Mel")
+
+
   )
